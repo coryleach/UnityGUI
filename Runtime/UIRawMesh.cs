615357827ccbb7382839fe713f62518d5343176e
Cory Leach <cory.leach@gmail.com>
2019-10-27 18:24:54 -0400

    UIRawMesh

---
+++ Runtime/UIRawMesh.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Gameframe.GUI
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class UIRawMesh : MonoBehaviour
+    {
+        private void Start()
+        {
+            Refresh();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            Refresh();
+        }
+#endif
+
+        private void Refresh()
+        {
+            var canvasRenderer = GetComponent<CanvasRenderer>();
+            var meshRenderer = GetComponent<MeshRenderer>();
+            var meshFilter = GetComponent<MeshFilter>();
+
+            if (canvasRenderer == null || meshRenderer == null || meshFilter == null)
+            {
+                return;
+            }
+
+            meshRenderer.enabled = false;
+            if (meshFilter.sharedMesh.isReadable)
+            {
+                canvasRenderer.SetMesh(meshFilter.sharedMesh);
+            }
+            else
+            {
+                Debug.LogErrorFormat("Mesh {0} must be marked as readable to be used with UIRawMesh component!",meshFilter.sharedMesh.name);
+            }
+            canvasRenderer.materialCount = 1;
+            canvasRenderer.SetMaterial(meshRenderer.sharedMaterials[0], 0);
+        }
+    }
+}
