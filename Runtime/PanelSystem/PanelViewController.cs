2ca835227529a37014ce6499dc3b080718539de6
Cory Leach <cory.leach@gmail.com>
2019-12-15 14:25:42 -0500

    Generics for PanelViewController implementations
    Added generic panel view controllers that provide a View property that returns a specific child view class.

--- Runtime/PanelSystem/PanelViewController.cs
+++ Runtime/PanelSystem/PanelViewController.cs
@@ -87,4 +87,82 @@ namespace Gameframe.GUI.PanelSystem
         }
         
     }
+    
+    public class PanelViewController<TPanelView> : IPanelViewController where TPanelView : PanelViewBase
+    {
+        private readonly PanelViewControllerBase baseController;
+        
+        public PanelViewController(PanelType type)
+        {
+            baseController = new PanelViewControllerBase(type, ViewDidLoad,ViewWillAppear,ViewDidAppear,ViewWillDisappear,ViewDidDisappear);
+        }
+
+        public PanelType PanelType => baseController.PanelType;
+
+        PanelViewBase IPanelViewController.View => baseController.View;
+
+        public TPanelView View => (TPanelView)baseController.View;
+        
+        public bool IsViewLoaded => baseController.IsViewLoaded;
+
+        public Task LoadViewAsync() => baseController.LoadViewAsync();
+
+        public Task HideAsync() => baseController.HideAsync();
+
+        public Task ShowAsync() => baseController.ShowAsync();
+
+        public IPanelViewContainer ParentViewContainer => baseController.ParentViewContainer;
+        
+        public void SetParentViewContainer(IPanelViewContainer parent) => baseController.SetParentViewContainer(parent);
+
+        protected virtual void ViewDidLoad()
+        {
+            
+        }
+
+        protected virtual void ViewWillAppear()
+        {
+            
+        }
+        
+        protected virtual void ViewDidAppear()
+        {
+            
+        }
+
+        protected virtual void ViewWillDisappear()
+        {
+            
+        }
+
+        protected virtual void ViewDidDisappear()
+        {
+            
+        }
+
+        private static SynchronizationContext mainSyncContext = null;
+        [RuntimeInitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            mainSyncContext = SynchronizationContext.Current;
+        }
+        
+        ~PanelViewController()
+        {
+            mainSyncContext?.Post((state) =>
+            {
+                if (!UnityEngine.Application.isPlaying)
+                {
+                    return;
+                }
+                var view = state as PanelViewBase;
+                if (view != null)
+                {
+                    UnityEngine.Object.Destroy(view.gameObject);
+                }
+            },baseController.View);
+        }
+        
+    }
+
 }
--- Runtime/PanelSystem/PanelViewControllerBehaviour.cs
+++ Runtime/PanelSystem/PanelViewControllerBehaviour.cs
@@ -8,6 +8,129 @@ namespace Gameframe.GUI.PanelSystem
     /// MonoBehaviour implementing IPanelViewController interface
     /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
     /// </summary>
+    public abstract class PanelViewControllerBehaviour<TPanelViewBase> : MonoBehaviour, IPanelViewController, IPanelViewContainer where TPanelViewBase : PanelViewBase
+    {
+        [SerializeField]
+        private PanelType panelType = null;
+
+        [SerializeField]
+        private PanelViewBase panelView = null;
+        
+        private PanelViewControllerBase baseController = null;
+
+        private PanelViewControllerBase BaseController => baseController ?? (baseController = CreateController());
+
+        protected virtual void Awake()
+        {
+            //Base controller is created 
+            //Create the base controller in awake if it hasn't been created yet
+            if (baseController == null)
+            {
+                baseController = CreateController();
+            }
+        }
+
+        private PanelViewControllerBase CreateController()
+        {
+            //Panel View always starts in the 'disappeared' state
+            //If we are supplying a serialized view then we need to enforce that rule here
+            if (panelView != null)
+            {
+                panelView.gameObject.SetActive(false);
+            }
+            
+            var controller = new PanelViewControllerBase(panelType,panelView, 
+                ViewDidLoad,
+                ViewWillAppear, 
+                ViewDidAppear, 
+                ViewWillDisappear, 
+                ViewDidDisappear);
+            
+            controller.SetParentViewContainer(this);
+
+            return controller;
+        }
+        
+        public PanelType PanelType => BaseController.PanelType;
+
+        PanelViewBase IPanelViewController.View => BaseController.View;
+
+        public TPanelViewBase View => (TPanelViewBase)BaseController.View;
+        
+        public Task LoadViewAsync() => BaseController.LoadViewAsync();
+
+        public bool IsViewLoaded => BaseController.IsViewLoaded;
+
+        public IPanelViewContainer ParentViewContainer => BaseController.ParentViewContainer;
+
+        private RectTransform rectTransform = null;
+
+        public RectTransform ParentTransform
+        {
+            get
+            {
+                if (rectTransform == null)
+                {
+                    rectTransform = gameObject.GetOrAddComponent<RectTransform>();
+                }
+                return rectTransform;
+            }
+        }
+
+        public void SetParentViewContainer(IPanelViewContainer parent) => BaseController.SetParentViewContainer(parent);
+
+        [ContextMenu("Show")]
+        public async void Show()
+        {
+            await ShowAsync();
+        }
+
+        public Task ShowAsync() => BaseController.ShowAsync();
+
+        [ContextMenu("Hide")]
+        public async void Hide()
+        {
+            await HideAsync();
+        }
+
+        public Task HideAsync() => BaseController.HideAsync();
+        
+        protected virtual void ViewDidLoad()
+        {
+        }
+        
+        protected virtual void ViewWillAppear()
+        {
+        }
+
+        protected virtual void ViewDidAppear()
+        {
+        }
+
+        protected virtual void ViewWillDisappear()
+        {
+        }
+
+        protected virtual void ViewDidDisappear()
+        {
+        }
+
+        #if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+            if (panelType != null)
+            {
+                name = $"PanelController - {panelType.name}";
+            }
+        }
+        #endif
+
+    }
+    
+    /// <summary>
+    /// MonoBehaviour implementing IPanelViewController interface
+    /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
+    /// </summary>
     public class PanelViewControllerBehaviour : MonoBehaviour, IPanelViewController, IPanelViewContainer
     {
         [SerializeField]
