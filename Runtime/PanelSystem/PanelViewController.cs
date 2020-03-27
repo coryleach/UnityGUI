cfd93954ed80dd2054319794fa70104d8cc0674e
Cory <cory.leach@gmail.com>
2020-03-27 02:41:44 -0400

    Update PanelViewController.cs
    Fixing AOT compile issue.

--- Runtime/PanelSystem/PanelViewController.cs
+++ Runtime/PanelSystem/PanelViewController.cs
@@ -64,27 +64,38 @@ namespace Gameframe.GUI.PanelSystem
             
         }
 
-        private static SynchronizationContext mainSyncContext = null;
+        internal static SynchronizationContext mainSyncContext = null;
+        
         [RuntimeInitializeOnLoadMethod]
         private static void Initialize()
         {
             mainSyncContext = SynchronizationContext.Current;
         }
+
+        internal static void CleanupDestroyedPanel(object state)
+        {
+            //If we're not running we don't need to do anything
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+            //If we have a panel view and we're running we should be able to destroy it
+            //This is so that we could allocate controllers on the fly
+            //when they're potenitally popped from a stack and cleaned up 
+            //they will then clean up the views they created as well.
+            var view = state as PanelViewBase;
+            if (view != null)
+            {
+                Object.Destroy(view.gameObject);
+            }
+        }
         
+        /// <summary>
+        /// This destructor will destroy the panel view that it may have instantiated
+        /// </summary>
         ~PanelViewController()
         {
-            mainSyncContext?.Post((state) =>
-            {
-                if (!UnityEngine.Application.isPlaying)
-                {
-                    return;
-                }
-                var view = state as PanelViewBase;
-                if (view != null)
-                {
-                    UnityEngine.Object.Destroy(view.gameObject);
-                }
-            },baseController.View);
+            mainSyncContext?.Post(CleanupDestroyedPanel,baseController.View);
         }
         
     }
@@ -141,28 +152,14 @@ namespace Gameframe.GUI.PanelSystem
         {
             
         }
-
-        private static SynchronizationContext mainSyncContext = null;
-        [RuntimeInitializeOnLoadMethod]
-        private static void Initialize()
-        {
-            mainSyncContext = SynchronizationContext.Current;
-        }
         
+        /// <summary>
+        /// This destructor will destroy the panel view that it may have instantiated
+        /// </summary>
         ~PanelViewController()
         {
-            mainSyncContext?.Post((state) =>
-            {
-                if (!Application.isPlaying)
-                {
-                    return;
-                }
-                var view = state as PanelViewBase;
-                if (view != null)
-                {
-                    Object.Destroy(view.gameObject);
-                }
-            },baseController.View);
+            //Using internal variable & method from PanelViewController to avoid AOT compile issues with this generic class
+            PanelViewController.mainSyncContext?.Post(PanelViewController.CleanupDestroyedPanel,baseController.View);
         }
         
     }
