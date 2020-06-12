7859ca3051f9bc841d058ad3d214511d61c04fd8
coryleach <cory.leach@gmail.com>
2020-06-12 15:27:05 -0400

    Update HorizontalOrVerticalPagedLayoutGroup.cs

--- Runtime/Layout/HorizontalOrVerticalPagedLayoutGroup.cs
+++ Runtime/Layout/HorizontalOrVerticalPagedLayoutGroup.cs
@@ -6,16 +6,12 @@ namespace Gameframe.GUI.Layout
 
   public abstract class HorizontalOrVerticalPagedLayoutGroup : LayoutGroup, ILayoutSelfController
   {
-
-    public float spacing = 0;
+    [SerializeField]
+    protected float spacing;
 
     [SerializeField]
     protected RectTransform viewport;
-    public RectTransform Viewport
-    {
-      get { return viewport; }
-    }
-
+    public RectTransform Viewport => viewport;
   }
 
 }
