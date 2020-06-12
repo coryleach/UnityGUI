41da1e6bd9f770c36e52fd7cf9e04f958446535b
coryleach <cory.leach@gmail.com>
2020-06-12 16:19:38 -0400

    Update PopPanelTransitionPresenter.cs

--- Runtime/PanelSystem/TransitionSystemExtensions/PopPanelTransitionPresenter.cs
+++ Runtime/PanelSystem/TransitionSystemExtensions/PopPanelTransitionPresenter.cs
@@ -9,11 +9,11 @@ namespace Gameframe.GUI.PanelSystem
     /// </summary>
     public class PanelStackPopTransition : ITransitionPresenter
     {
-        private readonly PanelStackSystem stack;
+        private readonly PanelStackSystem _stack;
 
         public PanelStackPopTransition(PanelStackSystem stack)
         {
-            this.stack = stack;
+            _stack = stack;
         }
     
         public Task StartTransitionAsync()
@@ -23,11 +23,12 @@ namespace Gameframe.GUI.PanelSystem
 
         public void TransitionProgress(float progress)
         {
+            //Progress display not currently supported for this transition presenter
         }
 
         public async Task FinishTransitionAsync()
         {
-            await stack.PopAsync(); 
+            await _stack.PopAsync(); 
         }
     }    
 }
