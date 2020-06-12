6b2f526691d3a3f5f29ed8c2199ec85db63f5e7c
coryleach <cory.leach@gmail.com>
2020-06-12 16:49:13 -0400

    Resolving some redundant code issues with PanelViewControllerBehaviour

--- Runtime/PanelSystem/PanelViewControllerBehaviour.cs
+++ Runtime/PanelSystem/PanelViewControllerBehaviour.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 namespace Gameframe.GUI.PanelSystem
 {
-    /// <summary>
-    /// MonoBehaviour implementing IPanelViewController interface
-    /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
-    /// </summary>
-    public abstract class PanelViewControllerBehaviour<TPanelViewBase> : MonoBehaviour, IPanelViewController, IPanelViewContainer where TPanelViewBase : PanelViewBase
+    public abstract class PanelViewControllerBehaviourBase<TPanelViewBase> : MonoBehaviour, IPanelViewController, IPanelViewContainer
     {
-        [SerializeField]
-        private PanelType panelType;
+        [SerializeField] 
+        protected PanelType panelType;
 
-        [SerializeField]
-        private PanelViewBase panelView;
+        [SerializeField, Help("When PanelView is null the prefab assigned to the Panel Type will be instantiated and used at runtime")]
+        protected PanelViewBase panelView;
         
         private PanelViewControllerBase baseController;
 
-        private PanelViewControllerBase BaseController => baseController ?? (baseController = CreateController());
+        internal PanelViewControllerBase BaseController => baseController ?? (baseController = CreateController());
 
         protected virtual void Awake()
         {
@@ -56,140 +52,9 @@ namespace Gameframe.GUI.PanelSystem
         public PanelType PanelType => BaseController.PanelType;
         
         PanelViewBase IPanelViewController.View => BaseController.View;
-
-        public TPanelViewBase View => (TPanelViewBase)BaseController.View;
         
-        public Task LoadViewAsync() => BaseController.LoadViewAsync();
+        public abstract TPanelViewBase View { get; }
 
-        public bool IsViewLoaded => BaseController.IsViewLoaded;
-
-        public IPanelViewContainer ParentViewContainer => BaseController.ParentViewContainer;
-
-        private RectTransform rectTransform;
-
-        public RectTransform ParentTransform
-        {
-            get
-            {
-                if (rectTransform == null)
-                {
-                    rectTransform = gameObject.GetOrAddComponent<RectTransform>();
-                }
-                return rectTransform;
-            }
-        }
-
-        public void SetParentViewContainer(IPanelViewContainer parent) => BaseController.SetParentViewContainer(parent);
-
-        public async void Show()
-        {
-            await ShowAsync().ConfigureAwait(false);
-        }
-        
-        public async void Show(bool immediate)
-        {
-            await ShowAsync(immediate).ConfigureAwait(false);
-        }
-
-        public Task ShowAsync(bool immediate = false) => BaseController.ShowAsync(immediate);
-
-        public async void Hide()
-        {
-            await HideAsync().ConfigureAwait(false);
-        }
-        
-        public async void Hide(bool immediate)
-        {
-            await HideAsync(immediate).ConfigureAwait(false);
-        }
-
-        public Task HideAsync(bool immediate = false) => BaseController.HideAsync(immediate);
-        
-        protected virtual void ViewDidLoad()
-        {
-        }
-        
-        protected virtual void ViewWillAppear()
-        {
-        }
-
-        protected virtual void ViewDidAppear()
-        {
-        }
-
-        protected virtual void ViewWillDisappear()
-        {
-        }
-
-        protected virtual void ViewDidDisappear()
-        {
-        }
-
-        #if UNITY_EDITOR
-        protected virtual void OnValidate()
-        {
-            if (panelType != null)
-            {
-                name = $"PanelController - {panelType.name}";
-            }
-        }
-        #endif
-
-    }
-    
-    /// <summary>
-    /// MonoBehaviour implementing IPanelViewController interface
-    /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
-    /// </summary>
-    public class PanelViewControllerBehaviour : MonoBehaviour, IPanelViewController, IPanelViewContainer
-    {
-        [SerializeField] 
-        private PanelType panelType;
-
-        [SerializeField, Help("When PanelView is null the prefab assigned to the Panel Type will be instantiated and used at runtime")]
-        private PanelViewBase panelView;
-        
-        private PanelViewControllerBase baseController;
-
-        private PanelViewControllerBase BaseController => baseController ?? (baseController = CreateController());
-
-        protected virtual void Awake()
-        {
-            //Base controller is created 
-            //Create the base controller in awake if it hasn't been created yet
-            if (baseController == null)
-            {
-                baseController = CreateController();
-            }
-        }
-
-        private PanelViewControllerBase CreateController()
-        {
-            //Panel View always starts in the 'disappeared' state
-            //If we are supplying a serialized view then we need to enforce that rule here
-            if (panelView != null)
-            {
-                panelView.gameObject.SetActive(false);
-            }
-            
-            var controller = new PanelViewControllerBase(panelType,panelView, 
-                ViewDidLoad,
-                ViewWillAppear, 
-                ViewDidAppear, 
-                ViewWillDisappear, 
-                ViewDidDisappear);
-            
-            controller.SetParentViewContainer(this);
-
-            return controller;
-        }
-
-        public PanelViewControllerState State => BaseController.State;
-        
-        public PanelType PanelType => BaseController.PanelType;
-
-        public PanelViewBase View => BaseController.View;
-        
         public Task LoadViewAsync() => BaseController.LoadViewAsync();
 
         public bool IsViewLoaded => BaseController.IsViewLoaded;
@@ -271,7 +136,24 @@ namespace Gameframe.GUI.PanelSystem
             }
         }
         #endif
-
+    }
+        
+    /// <summary>
+    /// MonoBehaviour implementing IPanelViewController interface
+    /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
+    /// </summary>
+    public abstract class PanelViewControllerBehaviour<TPanelViewBase> : PanelViewControllerBehaviourBase<TPanelViewBase> where TPanelViewBase : PanelViewBase
+    {
+        public override TPanelViewBase View => (TPanelViewBase)BaseController.View;
+    }
+    
+    /// <summary>
+    /// MonoBehaviour implementing IPanelViewController interface
+    /// internally this is just a humble object wrapper for PanelViewControllerBase which does the heavy lifting
+    /// </summary>
+    public class PanelViewControllerBehaviour : PanelViewControllerBehaviourBase<PanelViewBase>
+    {
+        public override PanelViewBase View { get; }
     }
 }
 
