88ca38c67dbcf3a9b6532a913d6d5c795485f8ae
Cory Leach <cory@pracystudios.com>
2023-01-29 15:28:22 -0600

    Update ScriptablePanelSwapSystem.cs
    Clean up

--- Runtime/PanelSystem/ScriptablePanelSwapSystem.cs
+++ Runtime/PanelSystem/ScriptablePanelSwapSystem.cs
@@ -7,28 +7,28 @@ namespace Gameframe.GUI.PanelSystem
     [CreateAssetMenu(menuName = "Gameframe/PanelSystem/PanelSwapSystem")]
     public class ScriptablePanelSwapSystem : ScriptableObject, IPanelSwapSystem
     {
-        private PanelSwapSystem system = new PanelSwapSystem();
+        private readonly PanelSwapSystem _system = new PanelSwapSystem();
 
-        public UnityEvent OnSwap => system.OnSwap;
+        public UnityEvent OnSwap => _system.OnSwap;
 
         private void OnEnable()
         {
             //Clearing listeners because ScriptableObject will hold onto old subscribers
             //when running in editor
-            system.OnSwap.RemoveAllListeners();
+            _system.OnSwap.RemoveAllListeners();
         }
 
-        public IPanelViewController CurrentViewController => system.CurrentViewController;
+        public IPanelViewController CurrentViewController => _system.CurrentViewController;
 
-        public void AddController(IPanelSwapController controller) => system.AddController(controller);
+        public void AddController(IPanelSwapController controller) => _system.AddController(controller);
 
-        public void RemoveController(IPanelSwapController controller) => system.RemoveController(controller);
+        public void RemoveController(IPanelSwapController controller) => _system.RemoveController(controller);
 
-        public void Show(IPanelViewController controller) => system.Show(controller);
+        public void Show(IPanelViewController controller) => _system.Show(controller);
 
         public async Task ShowAsync(IPanelViewController panelViewController)
         {
-            await system.ShowAsync(panelViewController);
+            await _system.ShowAsync(panelViewController);
         }
     }
 }
